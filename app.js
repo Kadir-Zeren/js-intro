559f11eb28bcc1a6c7da03b3715e86488fc2c80f
kadir-zeren <k4dzeren@gmail.com>
2024-02-02 17:57:32 +0300

    variables

--- app.js
+++ app.js
@@ -1,10 +1,46 @@
-console.log("app js");
+// console.log("app js");
 
-alert("bu bir alert dir");
-karar = confirm("eminmisin");
-console.log(karar);
-console.log("alertden sonraki komut");
+// alert("bu bir alert dir");
+// karar = confirm("eminmisin");
+// console.log(karar);
+// console.log("alertden sonraki komut");
 
-console.warn("this is danger");
+// console.warn("this is danger");
 
-console.error("this is error");
+// console.error("this is error");
+
+console.log("APP JS RUNNING");
+
+const pi = 3.14;
+console.log(pi);
+console.log(typeof pi);
+
+const sayi1 = "3";
+console.log(typeof sayi1);
+
+const deneme = 4;
+console.log(deneme);
+
+const number1 = 5;
+
+const varMi = true;
+const isOpen = false;
+console.log(isOpen, typeof isOpen);
+
+let language = "Java";
+console.log(language, typeof language);
+
+language = "Javascript";
+console.log(language);
+
+language = true;
+console.log(language);
+
+language = 3.14;
+console.log(language);
+
+let adet;
+console.log(adet);
+
+adet = "23";
+console.log(typeof adet);
