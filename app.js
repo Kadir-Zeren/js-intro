d871f451d622471822b045a980c3084f793989da
kadir-zeren <k4dzeren@gmail.com>
2024-02-02 17:41:46 +0300

    output

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("app js");
+
+alert("bu bir alert dir");
+karar = confirm("eminmisin");
+console.log(karar);
+console.log("alertden sonraki komut");
+
+console.warn("this is danger");
+
+console.error("this is error");
